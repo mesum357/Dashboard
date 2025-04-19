f846bc9cc8cc4e6bf6dd79f2c5f5df5ebf9b5072
mesum357 <i.mesumabbas@gmail.com>
2025-04-19 14:44:15 +0500

    third

--- app.js
+++ app.js
@@ -393,7 +393,7 @@ Upload.find({})
 
 
 
-})
+
 
 
 app.get("/previous-data",function(req,res){
@@ -548,6 +548,7 @@ app.use((err, req, res, next) => {
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function() {
-    console.log("Server started on port " + PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 });
